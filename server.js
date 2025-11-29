eea8c81d695a82b76dfcbd014638695d031f4146
Harsh <vishwaharsh10@gmail.com>
2025-11-29 22:17:05 +0530

    Inkbyte version 1.4

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
 
 // Connect to Database
 connectDB();
